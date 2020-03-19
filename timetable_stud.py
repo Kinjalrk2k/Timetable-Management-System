2cd73b93734a6dee7333d0501e4177e2fec9a2fe
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-03-19 23:06:57 +0530

    Added Student Timetable window

---
+++ timetable_stud.py
@@ -0,0 +1,215 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import sqlite3
+
+days = 5
+periods = 6
+recess_break_aft = 3 # recess after 3rd Period
+section = None
+butt_grid = []
+
+
+period_names = list(map(lambda x: 'Period ' + str(x), range(1, 6+1)))
+day_names = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday']
+
+
+
+def select_sec():
+    global section
+    section = str(combo1.get())
+    print(section)
+    update_table(section)
+
+
+
+def update_table(sec):
+    for i in range(days):
+        for j in range(periods):
+            cursor = conn.execute(f"SELECT SUBCODE, FINI FROM SCHEDULE\
+                WHERE DAYID={i} AND PERIODID={j} AND SECTION='{sec}'")
+            cursor = list(cursor)
+            print(cursor)
+            if len(cursor) != 0:
+                butt_grid[i][j]['text'] = str(cursor[0][0]) + '\n' + str(cursor[0][1])
+                butt_grid[i][j].update()
+                print(i, j, cursor[0][0])
+            else:
+                butt_grid[i][j]['text'] = "No Class"
+                butt_grid[i][j].update()
+
+
+
+def process_button(d, p, sec):
+    details = tk.Tk()
+    cursor = conn.execute(f"SELECT SUBCODE, FINI FROM SCHEDULE\
+                WHERE ID='{section+str((d*periods)+p)}'")
+    cursor = list(cursor)
+    if len(cursor) != 0:
+        subcode = str(cursor[0][0]) 
+        fini =  str(cursor[0][1])
+
+        cur1 = conn.execute(f"SELECT SUBNAME, SUBTYPE FROM SUBJECTS\
+            WHERE SUBCODE='{subcode}'")
+        cur1 = list(cur1)
+        subname = str(cur1[0][0])
+        subtype = str(cur1[0][1])
+
+        cur2 = conn.execute(f"SELECT NAME, EMAIL FROM FACULTY\
+            WHERE INI='{fini}'")
+        cur2 = list(cur2)
+        fname = str(cur2[0][0])
+        femail = str(cur2[0][1]) 
+
+        if subtype == 'T':
+            subtype = 'Theory'
+        elif subtype == 'P':
+            subtype = 'Practical'
+
+        print(subcode, fini, subname, subtype, fname, femail)
+
+        tk.Label(details, text='Day: '+day_names[d], font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Period: '+str(p+1), font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Subject Code: '+subcode, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Subect Name: '+subname, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Subject Type: '+subtype, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Faculty Initials: '+fini, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Faculty Name: '+fname, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+        tk.Label(details, text='Faculty Email: '+femail, font=('Consolas'), anchor="w").pack(expand=1, fill=tk.X)
+
+
+
+def student_tt_frame(tt, sec):
+    global butt_grid
+
+    table = tk.Frame(tt)
+    table.pack()
+
+    first_half = tk.Frame(table)
+    first_half.pack(side='left')
+
+    recess_frame = tk.Frame(table)
+    recess_frame.pack(side='left')
+
+    second_half = tk.Frame(table)
+    second_half.pack(side='left')
+
+    recess = tk.Label(
+        recess_frame,
+        text='R\n\nE\n\nC\n\nE\n\nS\n\nS',
+        font=('Consolas', 18, 'italic'),
+        width=3,
+        relief='sunken'
+    )
+    recess.pack()
+
+    for i in range(days):
+        b = tk.Label(
+            first_half,
+            text=day_names[i],
+            font=('Consolas', 12, 'bold'),
+            width=9,
+            height=2,
+            bd=5,
+            relief='raised'
+        )
+        b.grid(row=i+1, column=0)
+
+    for i in range(periods):
+        if i < recess_break_aft:
+            b = tk.Label(first_half)
+            b.grid(row=0, column=i+1)
+        else:
+            b = tk.Label(second_half)
+            b.grid(row=0, column=i)
+
+        b.config(
+            text=period_names[i],
+            font=('Consolas', 12, 'bold'),
+            width=9,
+            height=1,
+            bd=5,
+            relief='raised'
+        )
+
+    for i in range(days):
+        b = []
+        for j in range(periods):
+            if j < recess_break_aft:
+                bb = tk.Button(first_half)
+                bb.grid(row=i+1, column=j+1)
+            else:
+                bb = tk.Button(second_half)
+                bb.grid(row=i+1, column=j)
+
+            bb.config(
+                text='Hello World!',
+                font=('Consolas', 10),
+                width=13,
+                height=3,
+                bd=5,
+                relief='raised',
+                wraplength=80,
+                justify='center',
+                command=lambda x=i, y=j, z=sec: process_button(x, y, z)
+            )
+            b.append(bb)
+
+        butt_grid.append(b)
+        # print(b)
+        b = []
+
+    print(butt_grid[0][1], butt_grid[1][1])
+    update_table(sec)
+
+
+
+conn = sqlite3.connect(r'files/timetable.db')
+if __name__ == "__main__":
+    
+    # connecting database
+
+    tt = tk.Tk()
+
+    title_lab = tk.Label(
+        tt,
+        text='T  I  M  E  T  A  B  L  E',
+        font=('Consolas', 20, 'bold'),
+        pady=5
+    )
+    title_lab.pack()
+
+    student_tt_frame(tt, section)
+
+    sec_select_f = tk.Frame(tt, pady=15)
+    sec_select_f.pack()
+
+    tk.Label(
+        sec_select_f,
+        text='Select section:  ',
+        font=('Consolas', 12, 'bold')
+    ).pack(side=tk.LEFT)
+
+    cursor = conn.execute("SELECT DISTINCT SECTION FROM STUDENT")
+    sec_li = [row[0] for row in cursor]
+    # sec_li.insert(0, 'NULL')
+    print(sec_li)
+    combo1 = ttk.Combobox(
+        sec_select_f,
+        values=sec_li,
+    )
+    combo1.pack(side=tk.LEFT)
+    combo1.current(0)
+
+    b = tk.Button(
+        sec_select_f,
+        text="OK",
+        font=('Consolas', 12, 'bold'),
+        padx=10,
+        command=select_sec
+    )
+    b.pack(side=tk.LEFT, padx=10)
+    b.invoke()
+
+
+    tt.mainloop()
